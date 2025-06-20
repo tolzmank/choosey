c3afbf6a45259546bbbfe6b59ad6c8fb2e39b89d
tolzmank <tolzmank@oregonstate.edu>
2025-06-19 18:10:22 -1000

    Added:
    Implemented GGloud datastore for storing user created stories.
    
    Support for displaying on mobile device.
    
    Firebase user login/account creation/authentication.
    
    Menu bar for buttons: login, logout, account, and My Stories. Where the user will be able to save/view/delete/update their stories.

--- app.py
+++ app.py
@@ -4,34 +4,82 @@ import random
 import re
 import threading
 import time
-from difflib import SequenceMatcher
+import uuid
+
 import json
 from dotenv import load_dotenv
 load_dotenv()
 
+# Google Cloud Datastore
+from google.cloud import datastore
+ds_client = datastore.Client(project="choosey-463422")
+
+import firebase_admin
+from firebase_admin import auth as firebase_auth, credentials
+
+cred = credentials.Certificate('choosey-463422-firebase-adminsdk.json')
+firebase_admin.initialize_app(cred)
+
 
 # OpenAI for answer bot
-from openai.types.chat import ChatCompletionMessage
+#from openai.types.chat import ChatCompletionMessage
 from openai import OpenAI
-#from openai.types import ResponseFormat
-#import openai
 openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-from flask import Flask, render_template, redirect, url_for, request, jsonify
+from flask import Flask, render_template, redirect, url_for, request, jsonify, session
 app = Flask(__name__)
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
 
-story_set = {}
 
 @app.route('/')
 def index():
-
-    return render_template('index.html',
+    story_id = session.get('story_id')
+    if story_id:
+        story_set = get_story(story_id)
+    else:
+        story_set = {}
+    
+    show_login = session.pop('show_login_form', False)
+    return render_template('index.html', 
+                           show_login=show_login,
+                           user=session.get('user'), 
                            story_set=story_set
                            )
 
-@app.route('/setup', methods=['POST'])
-def setup():
-    global story_set
+
+
+@app.route('/show_login', methods=['GET'])
+def show_login():
+    session['show_login_form'] = True
+    return redirect(url_for('index'))
+
+@app.route('/hide_login', methods=['GET'])
+def hide_login():
+    session['show_login_form'] = False
+    return redirect(url_for('index'))
+
+
+@app.route('/session_login', methods=['POST'])
+def session_login():
+    data = request.get_json()
+    id_token = data.get('idToken')
+    decoded = firebase_auth.verify_id_token(id_token)
+    session['user'] = { 'uid': decoded['uid'], 'email': decoded.get('email') }
+    #return ('', 204)
+    return redirect(url_for('index'))
+
+
+@app.route('/session_logout', methods=['GET'])
+def session_logout():
+    session.clear()
+    #return ('', 204)
+    return redirect(url_for('index'))
+
+
+@app.route('/create_story', methods=['POST'])
+def create_story():
+    user = session.get('user')
+
     genre = request.form.get('genre')
     length = request.form.get('length')
     control = request.form.get('control')
@@ -47,49 +95,116 @@ def setup():
         'story': []
     }
     story_set = get_next_story_block(story_set, None)
-    return redirect(url_for('index'))
+    story_id = save_story_db(story_set)
+    session['story_id'] = story_id
+    return render_template('index.html', user=user, story_set=story_set)
+
+
+def save_story_db(story_set):
+    if story_set:
+        entity = datastore.Entity(key=ds_client.key('Story'), exclude_from_indexes=['story'])
+        entity.update({
+            'genre': story_set['genre'],
+            'length': story_set['length'],
+            'control': story_set['control'],
+            'spice': story_set['spice'],
+            'persona': story_set['persona'],
+            'story': json.dumps(story_set['story'])
+        })
+        ds_client.put(entity)
+        print('Story saved with ID:', entity.key.id)
+        story_id = entity.key.id
+        return story_id
 
 
 @app.route('/choose_path', methods=['POST'])
 def choose_path():
-    global story_set
+    user = session.get('user')
+    story_id = session.get('story_id')
+    story_set = get_story(story_id)
+
     decision = request.form['decision']
     next = request.form['next']
     choice = {'decision': decision, 'next': next}
-    print('CHOICE >>> ', choice)
     story_set = get_next_story_block(story_set, choice)
-    print('STORY BLOCKS LENGTH: ', len(story_set['story']))
-    return redirect(url_for('index'))
+
+    update_story_db(story_id, story_set)
+
+    return render_template('index.html', user=user, story_set=story_set)
+
+
+def update_story_db(story_id, story_set):
+    key = ds_client.key('Story', story_id)
+    entity = ds_client.get(key)
+    if entity:
+        entity['story'] = json.dumps(story_set['story'])
+        ds_client.put(entity)
+        print('Story updated with ID:', story_id)
 
 
 @app.route('/start_over', methods=['POST'])
 def start_over():
-    global story_set
+    story_id = session.get('story_id')
+    story_set = get_story(story_id)
     initial_story_block = story_set['story'][0]
     story_set['story'] = [initial_story_block]
+    update_story_db(story_id, story_set)
     return redirect(url_for('index'))
 
 
 @app.route('/go_back', methods=['POST'])
 def go_back():
-    global story_set
+    story_id = session.get('story_id')
+    story_set = get_story(story_id)
     last_story_block = story_set['story'][-1]
     story_set['story'].remove(last_story_block)
+    update_story_db(story_id, story_set)
     return redirect(url_for('index'))
 
 
 @app.route('/reset', methods=['POST'])
 def reset():
-    global story_set
-    story_set = {}
+    story_id = session.get('story_id')
+    if story_id:
+        key = ds_client.key('Story', story_id)
+        ds_client.delete(key)
+        print(f'Story ID: {story_id} deleted')
+    session['story_id'] = None
     return redirect(url_for('index'))
 
 
-@app.route('/delete_stories', methods=['POST'])
-def delete_stories():
-    global story_set
-    story_set = {}
+@app.route('/delete_all_stories', methods=['POST'])
+def delete_all_stories():
+    query = ds_client.query(kind='Story')
+    keys = [entity.key for entity in query.fetch()]
+    ds_client.delete_multi(keys)
+    print(f'All stories deleted')
     return redirect(url_for('index'))
+
+
+@app.route('/delete_story/<int:story_id>', methods=['POST'])
+def delete_story(story_id):
+    if story_id:
+        key = ds_client.key('Story', story_id)
+        ds_client.delete(key)
+        print(f'Story ID: {story_id} deleted')
+    return redirect(url_for('index'))
+
+
+def get_story(story_id):
+    key = ds_client.key('Story', story_id)
+    entity = ds_client.get(key)
+    if entity:
+        story_set = {
+            'genre': entity.get('genre'),
+            'length': entity.get('length'),
+            'control': entity.get('control'),
+            'spice': entity.get('spice'),
+            'persona': entity.get('persona'),
+            'story': json.loads(entity.get('story', '[]'))
+        }
+        return story_set
+    return None
 
 
 def map_user_set(story_set):
@@ -149,6 +264,7 @@ def map_user_set(story_set):
 
 
 def get_next_story_block(story_set, choice=None):
+    print('Getting story block...')
     user_set = map_user_set(story_set)
     genre = user_set['genre']
     total_paragraphs = user_set['length']
@@ -260,6 +376,6 @@ def get_next_story_block(story_set, choice=None):
 
 
 if __name__ == "__main__":
-    #app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
-    app.run(debug=False)
+    #app.run(debug=True)
